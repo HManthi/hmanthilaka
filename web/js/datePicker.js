63350bc728a62bf699899aaf89e75dd98da2451e
User <User@DESKTOP-KVHG1E3>
2018-12-24 13:29:58 +0530

    datePicker JS file

---
+++ web/js/datePicker.js
@@ -0,0 +1,17 @@
+ $(function () {
+        $('#datetimepicker1').datepicker({
+            format: "dd/mm/yyyy",
+            language: "en",
+            autoclose: true,
+            todayHighlight: true
+        });
+    });
+    
+     $(function () {
+        $('#datetimepicker2').datepicker({
+            format: "dd/mm/yyyy",
+            language: "en",
+            autoclose: true,
+            todayHighlight: true
+        });
+    });
